d86bca754ef2fa0272223afe750b9619afb6310c
Isep Lutpi Nur <iseplutpinur7@gmail.com>
2023-03-06 22:52:03 +0700

    ganti nama folder Model jadi model dan Controller jadi controller

--- MInggu6/project/src/main/java/id/kawahedukasi/Controller/PesertaController.java
+++ MInggu6/project/src/main/java/id/kawahedukasi/Controller/PesertaController.java
@@ -1,20 +1,63 @@
 package id.kawahedukasi.Controller;
 
+import id.kawahedukasi.Model.Peserta;
+
+import javax.transaction.Transactional;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.HashMap;
+import java.util.Map;
 
 @Path("/peserta")
-@Produces(MediaType.TEXT_PLAIN)
+@Produces(MediaType.APPLICATION_JSON)
 public class PesertaController {
-
-    @Path("/{name}")
     @GET
-    public String inputPathParameter(@PathParam("name") String name){
-        return "Peserta Dengan Nama input path param: " + name;
+    public Response get(){
+        return Response.status(Response.Status.OK).entity(Peserta.findAll().list()).build();
     }
 
-    @GET
-    public String inputQueryParameter(@QueryParam("name") String name){
-        return "Peserta Dengan Nama input query param: " + name;
+    @POST
+    @Transactional
+    public Response post(Map<String, Object> request){
+        Peserta peserta = new Peserta();
+        peserta.name = request.get("name").toString();
+        peserta.email = request.get("email").toString();
+        peserta.phoneNumber = request.get("phoneNumber").toString();
+
+        // save
+        peserta.persist();
+        return Response.status(Response.Status.CREATED).entity(new HashMap<>()).build();
+    }
+    @PUT
+    @Path("/{id}")
+    @Transactional
+    public Response put(@PathParam("id") Long id, Map<String, Object> request){
+        Peserta peserta = Peserta.findById(id);
+
+        if(peserta == null){
+            return Response.status(Response.Status.BAD_REQUEST).build();
+        }
+
+        peserta.name = request.get("name").toString();
+        peserta.email = request.get("email").toString();
+        peserta.phoneNumber = request.get("phoneNumber").toString();
+
+        // save
+        peserta.persist();
+        return Response.status(Response.Status.CREATED).entity(new HashMap<>()).build();
+    }
+
+    @DELETE
+    @Path("/{id}")
+    @Transactional
+    public Response delete(@PathParam("id") Long id, Map<String, Object> request){
+        Peserta peserta = Peserta.findById(id);
+        if(peserta == null){
+            return Response.status(Response.Status.BAD_REQUEST).build();
+        }
+
+        peserta.delete();
+        return Response.status(Response.Status.OK).entity(new HashMap<>()).build();
     }
 }
